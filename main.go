9f20f5c61b4c721a7c2f76d2bc71ac36aab756d2
Tim Heuett <tim.heuett@gmail.com>
2019-03-22 21:10:49 -0700

    adds tls support with local cert

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -9,5 +10,18 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Nope!")
 	})
-	http.ListenAndServe(":80", nil)
+	forever := make(chan struct{})
+	go func() {
+		checkFatal(http.ListenAndServe(":80", nil))
+	}()
+	go func() {
+		checkFatal(http.ListenAndServeTLS(":443", "cert.pem", "key.pem", nil))
+	}()
+	<-forever
+}
+
+func checkFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
